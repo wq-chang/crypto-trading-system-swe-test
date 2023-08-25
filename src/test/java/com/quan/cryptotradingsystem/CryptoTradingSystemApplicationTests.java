50c27d68b43de894c3cee16315c8412b22a43251
Wing Quan <wingquan.chang@gmail.com>
2023-08-26 01:19:01 +0800

    initial commit

---
+++ src/test/java/com/quan/cryptotradingsystem/CryptoTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.cryptotradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
