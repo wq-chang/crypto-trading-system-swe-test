da4a47fa90a7a716d0438dc3d3ce4f4f10bdbde7
Wing Quan <wingquan.chang@gmail.com>
2023-08-26 13:44:58 +0800

    auto generate ddl

---
+++ src/main/java/com/quan/cryptotradingsystem/entity/Price.java
@@ -0,0 +1,61 @@
+package com.quan.cryptotradingsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity(name = "prices")
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String symbol;
+    private BigDecimal bid;
+    private BigDecimal bidSize;
+    private BigDecimal ask;
+    private BigDecimal askSize;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public BigDecimal getBid() {
+        return bid;
+    }
+
+    public void setBid(BigDecimal bid) {
+        this.bid = bid;
+    }
+
+    public BigDecimal getBidSize() {
+        return bidSize;
+    }
+
+    public void setBidSize(BigDecimal bidSize) {
+        this.bidSize = bidSize;
+    }
+
+    public BigDecimal getAsk() {
+        return ask;
+    }
+
+    public void setAsk(BigDecimal ask) {
+        this.ask = ask;
+    }
+
+    public BigDecimal getAskSize() {
+        return askSize;
+    }
+
+    public void setAskSize(BigDecimal askSize) {
+        this.askSize = askSize;
+    }
+}
