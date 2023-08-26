3171f4608c652e7938d0f62101b4f36bf4e5ee70
Wing Quan <wingquan.chang@gmail.com>
2023-08-27 04:15:27 +0800

    add scheduled fetch price job

---
+++ src/main/java/com/quan/cryptotradingsystem/job/FetchPriceJob.java
@@ -0,0 +1,26 @@
+package com.quan.cryptotradingsystem.job;
+
+import com.quan.cryptotradingsystem.service.PriceCommandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FetchPriceJob {
+
+    Logger logger = LoggerFactory.getLogger(FetchPriceJob.class);
+
+    private final PriceCommandService priceCommandService;
+
+    public FetchPriceJob(PriceCommandService priceCommandService) {
+        this.priceCommandService = priceCommandService;
+    }
+
+    @Scheduled(cron = "${app.quartz.fetch-price-cron}")
+    public void fetchPeriodically() {
+        logger.info("Start fetching price");
+        priceCommandService.fetchAndSaveLatestPrices();
+        logger.info("Finish fetching price");
+    }
+}
